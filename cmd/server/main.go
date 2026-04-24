34faae6c2dd893da91268b1a94fa1ff2182f590c
Eval-99 <ev1914jw@gmail.com>
2026-04-24 14:05:52 -0500

    Can now publish JSON to RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,9 @@ import (
 	"os/signal"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
@@ -15,10 +18,28 @@ func main() {
 	conn, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("Failed connection to RabbitMQ: %v", err)
+		return
 	}
 	defer conn.Close()
 
 	fmt.Println("Connection to RabbitMQ successful")
+
+	amqpChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed RabbitMQ to create RabbitMQ channel: %v", err)
+		return
+	}
+
+	err = pubsub.PublishJSON(
+		amqpChan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+	if err != nil {
+		log.Fatalf("Failed to publish JSON: %v", err)
+		return
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
